444112eb9e2524bd39364fb1b990f656580af00b
LohanLima <86078127+LohanLima@users.noreply.github.com>
2021-06-28 14:10:53 -0300

    Add files via upload

---
+++ CarroPe.java
@@ -0,0 +1,11 @@
+package CarroE;
+
+public class CarroPe {
+
+    public static void main(String[] args) {
+
+        Carro exercicioCarro = new Carro();
+
+        System.out.println(exercicioCarro.mostrandoQuant());
+    }
+}
